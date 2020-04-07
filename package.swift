9449d395ecf57d163aa0aa4e98a062b7834c1f12
Anthony Henderson <anthony.h.henderson@outlook.com>
2020-04-06 23:24:55 -0700

    Update package.swift

--- package.swift
+++ package.swift
@@ -1,1 +1,18 @@
 
+import PackageDescription
+
+let package = Package(
+    name: "Navajo-Swift",
+    products: [
+        .library(name: "Navajo-Swift", targets: ["Navajo-Swift"]),
+    ],
+//     dependencies: [
+//         .package(url: "https://github.com/phimage/Erik.git", .upToNextMajor(from: "5.0.0")),
+//         .package(url: "https://github.com/tid-kijyun/Kanna.git", .upToNextMajor(from: "5.0.0")),
+//         .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.4.7"))
+//     ],
+    targets: [
+        .target(name: "OAuthSwift", dependencies: [], path: "Sources"),
+        .testTarget(name: "OAuthSwiftTests", dependencies: ["OAuthSwift", "Erik", "Kanna", "Swifter"], path: "OAuthSwiftTests"),
+    ]
+)
