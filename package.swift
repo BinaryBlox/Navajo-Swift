c9313853f596cad0b61cc00aa97ac2992cc09424
Tony Henderson <anthony.h.henderson@gmail.com>
2020-04-07 12:27:03 -0700

    fixed swift pacakge

--- package.swift
+++ package.swift
@@ -6,13 +6,8 @@ let package = Package(
     products: [
         .library(name: "Navajo", targets: ["Navajo iOS"]),
     ],
-//     dependencies: [
-//         .package(url: "https://github.com/phimage/Erik.git", .upToNextMajor(from: "5.0.0")),
-//         .package(url: "https://github.com/tid-kijyun/Kanna.git", .upToNextMajor(from: "5.0.0")),
-//         .package(url: "https://github.com/httpswift/swifter.git", .upToNextMajor(from: "1.4.7"))
-//     ],
     targets: [
-        .target(name: "Navajo iOS", dependencies: [], path: "Sources"),
+        .target(name: "Navajo iOS", dependencies: [], path: "Sources")
         //.testTarget(name: "OAuthSwiftTests", dependencies: ["OAuthSwift", "Erik", "Kanna", "Swifter"], path: "OAuthSwiftTests"),
     ]
 )
